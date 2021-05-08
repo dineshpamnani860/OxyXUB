3d111644fc46d6d1fea03de0711c88ab384eef3c
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-08 15:41:33 +0530

    Add files via upload

---
+++ resources/startup/startup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# OxyX - UserBot
+# Copyright (C) 2020 OxyNotOp
+#
+# This file is a part of < https://github.com/OxyNotOp/OxyX-UB/ >
+# PLease read the GNU Affero General Public License in <https://www.github.com/OxyNotOp/OxyX-UB/blob/main/LICENSE/>.
+
+
+echo "
+             ▄℠︻̷̿┻̿═━一OxyXub
+
+      Visit @OxyXsupport for updates!!
+
+"
+
+python3 -m pyOxyX-UB
