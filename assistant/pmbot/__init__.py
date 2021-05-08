db46e921d100c1d51021540ae5117041d94c666c
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-08 17:45:18 +0530

    Add files via upload

--- assistant/pmbot/__init__.py
+++ assistant/pmbot/__init__.py
@@ -2,5 +2,5 @@ from pyUltroid.functions.asst_fns import *
 
 from .. import *
 
-OWNER_NAME = OxyXUB_bot.me.first_name
-OWNER_ID = OxyXUB_bot.me.id
+OWNER_NAME = ultroid_bot.me.first_name
+OWNER_ID = ultroid_bot.me.id
