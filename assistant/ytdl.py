42e641a360efe3e9d31c4d10bc21678daabc6e05
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-08 17:30:19 +0530

    Update ytdl.py

--- assistant/ytdl.py
+++ assistant/ytdl.py
@@ -11,7 +11,7 @@ import os
 import re
 import time
 
-from pyOxyXUB.functions.all import *
+from pyUltroid.functions.all import *
 from telethon import Button
 from telethon.errors import UserNotParticipantError
 from telethon.tl.types import DocumentAttributeAudio
